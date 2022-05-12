b9e528604861a237ab412d7fbaf15567c1a64305
Shayunak <shayanhamidi2000@gmail.com>
2022-05-12 18:02:00 +0430

    sensor acceleration and angle have benn calculated!

--- app/src/main/java/com/example/playground/MainActivity.java
+++ app/src/main/java/com/example/playground/MainActivity.java
@@ -24,38 +24,57 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private SensorManager sensorManager;
+    private SensorManager mSensorManager;
     private Sensor sensor;
     private int epoch = 0;
-    private List<Entry> x_entries = new ArrayList<>();
+    private boolean isRunning = false;
+    private LineChart chart;
+    private double Theta;
+    private double dt = .2;
+
     private List<Entry> y_entries = new ArrayList<>();
     private List<Entry> z_entries = new ArrayList<>();
 
+
+    private List<Entry> v_entries_y = new ArrayList<>();
+    private List<Entry> v_entries_z = new ArrayList<>();
+
+    private double v_y = 0;
+    private double v_z = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setup_chart();
+        this.chart = (LineChart) findViewById(R.id.chart_acc);
         setup_sensors();
     }
 
     private void setup_sensors(){
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    private double calculate_theta(float acceleration_z){
+        Theta = Math.acos(acceleration_z / 9.89);
+        return Theta*180/Math.PI;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        LineChart chart = (LineChart) findViewById(R.id.chart);
+        LineChart chart = (LineChart) findViewById(R.id.chart_acc);
         epoch ++;
-        x_entries.add(new Entry(epoch, event.values[0]));
         y_entries.add(new Entry(epoch, event.values[1]));
         z_entries.add(new Entry(epoch, event.values[2]));
-        Log.d("MainActivity","X accleration: " + String.valueOf(event.values[0]));
-        Log.d("MainActivity","Y accleration: " + String.valueOf(event.values[1]));
-        Log.d("MainActivity","Z accleration: " + String.valueOf(event.values[2]));
-        chart.invalidate();
+
+        Log.d("MainActivity","X acceleration: " + String.valueOf(event.values[0]));
+        Log.d("MainActivity","Y acceleration: " + String.valueOf(event.values[1]));
+        Log.d("MainActivity","Z acceleration: " + String.valueOf(event.values[2]));
+        draw_chart();
+
+
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Kiram Dahanet: " + String.valueOf((int)calculate_theta(event.values[2])));
     }
 
     @Override
@@ -64,18 +83,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void say_kir(View view){
-        TextView text_view = (TextView) findViewById(R.id.textView);
-        text_view.setText("Kirram Dahanet!");
+        Button button = (Button) findViewById(R.id.button);
+        if(isRunning){
+            mSensorManager.unregisterListener(this);
+            v_y = 0;
+            v_z = 0;
+            draw_chart();
+            button.setText("Start");
+        }else{
+            button.setText("Running...");
+            mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        isRunning = !isRunning;
     }
 
-    private void setup_chart(){
-        x_entries.add(new Entry(0, 0));
-        y_entries.add(new Entry(0 , 0));
-        z_entries.add(new Entry(0 , 0));
-        LineChart chart = (LineChart) findViewById(R.id.chart);
-        LineDataSet dataSet_x = new LineDataSet(this.x_entries, "X");
-        dataSet_x.setColor(Color.RED);
-        dataSet_x.setValueTextColor(Color.RED);
+    private void draw_chart(){
         LineDataSet dataSet_y = new LineDataSet(this.y_entries, "Y");
         dataSet_y.setColor(Color.GREEN);
         dataSet_y.setValueTextColor(Color.GREEN);
@@ -83,10 +105,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         dataSet_z.setColor(Color.YELLOW);
         dataSet_z.setValueTextColor(Color.YELLOW);
         ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(dataSet_x);
         dataSets.add(dataSet_y);
         dataSets.add(dataSet_z);
         LineData line = new LineData(dataSets);
         chart.setData(line);
+        chart.invalidate();
     }
 }
