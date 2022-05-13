7840385e7bb0270761d4eb341df01d43595ec679
Shayunak <shayanhamidi2000@gmail.com>
2022-05-14 00:09:08 +0430

    A big problem!

--- app/src/main/java/com/example/playground/MainActivity.java
+++ app/src/main/java/com/example/playground/MainActivity.java
@@ -25,89 +25,106 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
-    private Sensor sensor;
-    private int epoch = 0;
+    private Sensor mAccSensor;
+    private Sensor mLinSensor;
+    private int chart_window_size = 50;
     private boolean isRunning = false;
     private LineChart chart;
-    private double Theta;
-    private double dt = .2;
 
-    private List<Entry> y_entries = new ArrayList<>();
-    private List<Entry> z_entries = new ArrayList<>();
+    private double mCosTheta;
+    private double mSinTheta;
+    private double dt = .1;
 
-
-    private List<Entry> v_entries_y = new ArrayList<>();
-    private List<Entry> v_entries_z = new ArrayList<>();
-
-    private double v_y = 0;
-    private double v_z = 0;
+    private List<Double> y_acc = new ArrayList<>();
+    private List<Double> y_vel = new ArrayList<>();
+    private List<Entry> entries = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.chart = (LineChart) findViewById(R.id.chart_acc);
+        Button button = (Button) findViewById(R.id.button);
+        button.setText("Start");
         setup_sensors();
     }
 
     private void setup_sensors(){
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mLinSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        mAccSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     private double calculate_theta(float acceleration_z){
-        Theta = Math.acos(acceleration_z / 9.89);
-        return Theta*180/Math.PI;
+        mCosTheta = acceleration_z / 9.9;
+        mSinTheta = Math.sqrt(1.0 - Math.pow(mCosTheta, 2));
+        return Math.acos(mCosTheta)*180/Math.PI;
+    }
+
+    private void addDataPointToAccelerationEntries(double dataPoint_y) {
+        int number_of_data_points = y_acc.size();
+        y_acc.add(dataPoint_y);
+        if (number_of_data_points > chart_window_size)
+            y_acc.remove(0);
+    }
+
+    private void addDataPointToVelocityEntries(double dataPoint_y){
+        int number_of_data_points = y_vel.size();
+        y_vel.add(dataPoint_y);
+        if (number_of_data_points > chart_window_size)
+            y_vel.remove(0);
+    }
+
+    private double delete_noise(double data, int accuracy){
+        int rounded_data = (int)(data * Math.pow(10, 2));
+        return rounded_data / Math.pow(10, 2);
+    }
+
+    private double get_average(List<Double> list){
+        int iter = Math.min(10, list.size());
+        double sum = 0;
+        for (int i = list.size() - 1; i > list.size() - iter - 1; i--)
+            sum += list.get(i);
+        return sum / iter;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        LineChart chart = (LineChart) findViewById(R.id.chart_acc);
-        epoch ++;
-        y_entries.add(new Entry(epoch, event.values[1]));
-        z_entries.add(new Entry(epoch, event.values[2]));
-
-        Log.d("MainActivity","X acceleration: " + String.valueOf(event.values[0]));
-        Log.d("MainActivity","Y acceleration: " + String.valueOf(event.values[1]));
-        Log.d("MainActivity","Z acceleration: " + String.valueOf(event.values[2]));
-        draw_chart();
-
-
-
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("Kiram Dahanet: " + String.valueOf((int)calculate_theta(event.values[2])));
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            TextView textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Kiram Dahanet: " + String.valueOf((int)calculate_theta(event.values[2])));
+            Log.d("MainActivity","X acceleration: " + String.valueOf(event.values[2]));
+        }else if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
+            /*double acc_y = delete_noise(event.values[1], 1);
+            addDataPointToAccelerationEntries(acc_y);
+            addDataPointToVelocityEntries(get_average(y_acc)*dt);
+            entries.add(new Entry((float)(get_average(y_vel)*dt*mCosTheta), (float)(get_average(y_vel)*dt*mSinTheta)));
+            Log.d("MainActivity","Y acceleration: " + String.valueOf(acc_y));
+            draw_chart();*/
+        }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
     public void say_kir(View view){
         Button button = (Button) findViewById(R.id.button);
         if(isRunning){
             mSensorManager.unregisterListener(this);
-            v_y = 0;
-            v_z = 0;
-            draw_chart();
             button.setText("Start");
         }else{
             button.setText("Running...");
-            mSensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            mSensorManager.registerListener(this, mAccSensor, 100000);
+            mSensorManager.registerListener(this, mLinSensor, 100000);
         }
         isRunning = !isRunning;
     }
 
     private void draw_chart(){
-        LineDataSet dataSet_y = new LineDataSet(this.y_entries, "Y");
-        dataSet_y.setColor(Color.GREEN);
-        dataSet_y.setValueTextColor(Color.GREEN);
-        LineDataSet dataSet_z = new LineDataSet(this.z_entries, "Z");
-        dataSet_z.setColor(Color.YELLOW);
-        dataSet_z.setValueTextColor(Color.YELLOW);
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(dataSet_y);
-        dataSets.add(dataSet_z);
-        LineData line = new LineData(dataSets);
+        LineDataSet dataSet = new LineDataSet(entries, "location");
+        dataSet.setColor(Color.GREEN);
+        dataSet.setValueTextColor(Color.GREEN);
+        dataSet.setDrawCircles(false);
+        LineData line = new LineData(dataSet);
         chart.setData(line);
         chart.invalidate();
     }
